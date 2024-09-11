25678d4d57e1060f2155ac3a80b8953be642211b
Davpad <davidpadillavillen@gmail.com>
2024-09-11 19:04:45 +0200

    Añadida tabla de pregunta

--- backend/models.py
+++ backend/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(500), nullable=False)
     is_active = db.Column(db.Boolean(), nullable=True)
+    questions = db.relationship('Question', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -18,4 +19,37 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "username": self.username
+        }
+    
+class Question(db.Model):
+    __tablename__ = 'question'
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500), unique=True, nullable=False)
+    category = db.Column(db.String(240), unique=True, nullable=False)
+    logo = db.Column(db.String(240), unique=True, nullable=False)
+    answer1 = db.Column(db.String(240), unique=True, nullable=False)
+    option1 = db.Column(db.Boolean(), nullable=True)
+    answer2 = db.Column(db.String(240), unique=True, nullable=False)
+    option2 = db.Column(db.Boolean(), nullable=True)
+    answer3 = db.Column(db.String(240), unique=True, nullable=False)
+    option3 = db.Column(db.Boolean(), nullable=True)
+    reason = db.Column(db.String(240), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __repr__(self):
+        return f'<User {self.id}>'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "question": self.question,
+            "category": self.category,
+            "logo": self.logo,
+            "answer1": self.answer1,
+            "option1": self.option1,
+            "answer2": self.answer2,
+            "option2": self.option2,
+            "answer3": self.answer3,
+            "option3": self.option3,
+            "reason": self.reason
         }
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from models import db, User
+from models import db, User, Question
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
