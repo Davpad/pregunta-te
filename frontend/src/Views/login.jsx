acf948e9e346f9d71d70cbbcc3d4dff9f883600b
Davpad <davidpadillavillen@gmail.com>
2024-09-20 17:31:40 +0200

    Vista de login

--- frontend/src/Views/login.jsx
+++ frontend/src/Views/login.jsx
@@ -3,6 +3,18 @@ import React from "react";
 export const Login = () =>{
     
     return(
-        <div>Soy el login</div>
+        <form className="row g-3">
+        <div className="col-md-12">
+            <label for="inputEmail4" className="form-label">Email</label>
+            <input type="email" className="form-control" id="inputEmail"/>
+        </div>
+        <div className="col-md-12">
+            <label for="inputPassword4" className="form-label">Password</label>
+            <input type="password" className="form-control" id="inputPassword"/>
+        </div>
+        <div className="col-12">
+            <button type="submit" className="btn btn-primary">Login</button>
+        </div>
+    </form>  
     );
 };
