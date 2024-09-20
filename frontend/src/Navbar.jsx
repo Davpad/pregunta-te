6709e77a082e7a1219b5c04161abf014034029a9
Davpad <davidpadillavillen@gmail.com>
2024-09-20 12:34:31 +0200

    Enlazando el navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -2,28 +2,28 @@ import React, {Component} from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+    <nav className="navbar navbar-expand navbar-light bg-light">
+        <div className="container-fluid col-12">
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand" href="#">Hidden brand</a>
+                {/* <a className="navbar-brand" href="#">Hidden brand</a> */}
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <Link className="nav-link active" to="/preguntas">Preguntas</Link>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link disabled">Disabled</a>
                     </li>
                 </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                < div className="d-flex">
+                    <Link className="btn btn-primary" to="/signup" >Signup</Link>
+                    <Link className="btn btn-primary" to="/login" >Login</Link>
+                </div>
             </div>
         </div>
     </nav>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,14 +6,21 @@ import { Footer } from "./Footer.jsx";
 import {BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Views/home.jsx";
 import { Questions } from './Views/questions.jsx';
+import { Navbar } from './Navbar.jsx';
+import { Signup } from "./Views/signup.jsx";
+import { Login } from "./Views/login.jsx";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Navbar/>
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Questions />} path="/preguntas" />
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<Login />} path="/login" />
 
         </Routes>
       <Footer />
