60e259478e4a49559ce65f3b13abd1523de994c5
Davpad <davidpadillavillen@gmail.com>
2024-10-08 11:21:14 +0200

    Fixed postgres password

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Vader@localhost:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:vader@localhost:5432/postgres"
