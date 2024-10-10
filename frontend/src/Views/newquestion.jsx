b10be40b14c8087a02189fa892ad9f1336656cd5
Davpad <davidpadillavillen@gmail.com>
2024-10-10 13:04:09 +0200

    Feat: preparing the NewQuestion form

--- frontend/src/Views/newquestion.jsx
+++ frontend/src/Views/newquestion.jsx
@@ -1,50 +1,68 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
+import useAppContext from "../store/AppContext";
+import { useNavigate } from "react-router-dom";
+
 
 export const NewQuestion = () => {
+    const {store, actions} = useAppContext();
+
+    const [question,setQuestion] = useState();
+    const [category,setCategory] = useState();
+    const [option1,setOption1] = useState("false");
+    const [answer1,setAnswer1] = useState();
+    const [option2,setOption2] = useState("false");
+    const [answer2,setAnswer2] = useState();
+    const [option3,setOption3] = useState("false");
+    const [answer3,setAnswer3] = useState();
+    const [reason,setReason] = useState();
+    const navigate = useNavigate()
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        await actions.addQuestion(question, category, option1, answer1, option2, answer2, option3, answer3, reason, navigate)
+    }
 
     return (
-        <form className="row g-3">
+        <form className="row g-3" onSubmit={handleSubmit}>
             <div className="col-md-12">
                 <label for="question" className="form-label">Question</label>
-                <input type="text" className="form-control" id="question" placeholder="Question"/>
+                <input type="text" className="form-control" id="question" placeholder="Question" onChange={(event) => { setQuestion(event.target.value) }}/>
             </div>
             <div className="col-md-12">
                 <label for="category" className="form-label">Category</label>
-                <input type="text" className="form-control" id="category" placeholder="Category"/>
+                <input type="text" className="form-control" id="category" placeholder="Category" onChange={(event) => { setCategory(event.target.value) }}/>
             </div>
             <div className="col-2">
                 <div className="form-check mx-auto my-auto">
-                    <input className="form-check-input" type="checkbox" id="option1" value="true"/>
-                    <input className="form-check-input" type="hidden" id="option1" value="false"/>
+                    <input className="form-check-input" type="checkbox" id="option1" value="true" checked={option1 === "true"} onChange={(event) => { setOption1(event.target.value) }}/>
                 </div>
             </div>
             <div className="col-10">
                 <label for="answer1" className="form-label">Answer1</label>
-                <input type="text" className="form-control" id="answer1" placeholder="Answer1"/>
+                <input type="text" className="form-control" id="answer1" placeholder="Answer1" onChange={(event) => { setAnswer1(event.target.value) }}/>
             </div>
             <div className="col-2">
                 <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="option2" value="true"/>
-                    <input className="form-check-input" type="hidden" id="option2" value="false"/>
+                    <input className="form-check-input" type="checkbox" id="option2" value="true" checked={option2 === "true"} onChange={(event) => { setOption2(event.target.value) }}/>
                 </div>
             </div>
             <div className="col-10">
                 <label for="answer2" className="form-label">Answer2</label>
-                <input type="text" className="form-control" id="answer2" placeholder="Answer2" />
+                <input type="text" className="form-control" id="answer2" placeholder="Answer2" onChange={(event) => { setAnswer2(event.target.value) }}/>
             </div>
             <div className="col-2">
                 <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="option3" value="true"/>
-                    <input className="form-check-input" type="hidden" id="option3" value="false"/>
+                    <input className="form-check-input" type="checkbox" id="option3" value="true" checked={option3 === "true"} onChange={(event) => { setOption3(event.target.value) }}/>
                 </div>
             </div>
             <div className="col-10">
                 <label for="answer3" className="form-label">Answer3</label>
-                <input type="text" className="form-control" id="answer3" placeholder="Answer3" />
+                <input type="text" className="form-control" id="answer3" placeholder="Answer3" onChange={(event) => { setAnswer3(event.target.value) }}/>
             </div>
             <div className="col-12">
                 <label for="reason" className="form-label">Reason</label>
-                <input type="text" className="form-control" id="reason" placeholder="Reason" />
+                <input type="text" className="form-control" id="reason" placeholder="Reason" onChange={(event) => { setReason(event.target.value) }}/>
             </div>
             <div className="col-12">
                 <button type="submit" className="btn btn-primary">Crear pregunta</button>
