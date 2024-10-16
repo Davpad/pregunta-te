c178a74c23736bf995b214fc82cdea6a830d8e4a
Davpad <davidpadillavillen@gmail.com>
2024-10-16 21:37:54 +0200

    Feat: Adding variables in InfoQuestion

--- frontend/src/components/infoQuestion.jsx
+++ frontend/src/components/infoQuestion.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
 import useAppContext from "../store/AppContext";
+import { useNavigate } from "react-router-dom";
 
 
-export const InfoQuestion = () => {
+export const InfoQuestion = ({id, logo, category, question}) => {
+    const {store, actions} = useAppContext();
+    const navigate = useNavigate()
+    const token = localStorage.getItem("token");
+
 
     return (
-        <div className="card mb-3 w_100" >
+        <div className="card mb-3 w_100" key={id} >
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src="..." className="img-fluid rounded-start" alt="..."/>
+                    <img src={logo} className="img-fluid rounded-start" alt="..."/>
                 </div>
                 <div className="col-md-8">
-                    <div className="card-header"></div>
+                    <div className="card-header">{category}</div>
                     <div className="card-body">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text">{question}</p>
                         <div className="d-flex">
                             <button className="btn btn-danger">Delete</button>
                             <button className="btn btn-succes">Edit</button>
