ffe2f5720e8950e93b680c86ccf501fa5403c9a6
Davpad <davidpadillavillen@gmail.com>
2024-10-23 21:09:52 +0200

    Feat: showing reason after clicking

--- frontend/src/Views/question.jsx
+++ frontend/src/Views/question.jsx
@@ -9,17 +9,24 @@ export const Question = () =>{
     const [button1, setButton1] = useState("btn-outline-primary");
     const [button2, setButton2] = useState("btn-outline-primary");
     const [button3, setButton3] = useState("btn-outline-primary");
+    const [visible, setVisible] = useState("d-none");
+
 
     function changeButton1() {
         ((store.oneQuestion.option1 ==="true") ? setButton1("btn-success") : setButton1("btn-danger"));
+        setVisible("");
     }
 
     function changeButton2() {
         ((store.oneQuestion.option2 ==="true") ? setButton2("btn-success") : setButton2("btn-danger") )
+        setVisible("");
+
     }
 
     function changeButton3 () {
         ((store.oneQuestion.option3 ==="true") ? setButton3("btn-success") : setButton3("btn-danger") )
+        setVisible("");
+
     }
     
     useEffect(() => {
@@ -42,6 +49,10 @@ export const Question = () =>{
                     <button type="button" className={"btn "+button2} onClick={changeButton2}><b>{store.oneQuestion.answer2}</b></button>
                     <button type="button" className={"btn "+button3} onClick={changeButton3}><b>{store.oneQuestion.answer3}</b></button>
                 </div>
+            </div>
+            <div className={"mr-2 border border-primary "+visible}>
+                <h3>¿Por qué?</h3>
+                <p>{store.oneQuestion.reason}</p>
             </div>
         </div>
     );
